4cd59c45ca1fac88d167d0cbb16f5644e30ba21e
PREM KUMAR KONISETTY <137823075+Premkumarkonisetty@users.noreply.github.com>
2024-02-14 17:10:39 +0530

    Add files via upload

---
+++ sequence1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("enter range: ");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=1;j<=i*2-1;j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+}
