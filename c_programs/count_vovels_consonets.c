dc4bb66b45e866a7ab305b396f54ded8106d6a9c
PREM KUMAR KONISETTY <137823075+Premkumarkonisetty@users.noreply.github.com>
2024-02-18 21:36:24 +0530

    Delete c_programs directory

--- c_programs/count_vovels_consonets.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	char ch[20];
-	printf("Enter the string: ");
-	scanf("%s",ch);
-	printf("%s",ch);
-}
--- c_programs/string_arrangement.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	char name[30];
-	printf("Enter name of the string: ");
-	scanf("%s",name);
-	printf("%d",name[1]);
-
-}
